a13f72d4bbfaeb68cdf77dc2ec3e0c147a38530b
Bruno Sá <bruno.sa@sc.senai.br>
2023-06-30 14:40:59 -0300

    tentativa de correção

--- src/infrastructure/order/repository/sequilize/order.repository.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -2,6 +2,7 @@ import Order from "../../../../domain/checkout/entity/order";
 import OrderItemModel from "./order-item.model";
 import OrderModel from "./order.model";
 import OrderRepositoryInterface from "../../../../domain/checkout/repository/order-repository.interface"
+import OrderItem from "../../../../domain/checkout/entity/order_item";
 
 export default class OrderRepository implements OrderRepositoryInterface {
   async create(entity: Order): Promise<void> {
@@ -59,14 +60,32 @@ export default class OrderRepository implements OrderRepositoryInterface {
       throw new Error("Order not found");
     }
 
-    const order = new Order(orderModel.id, orderModel.customer_id, orderModel.items);
+    const order = new Order(
+      orderModel.id,
+      orderModel.customer_id,
+      orderModel.items.map((orderItemModel) => {
+        return new OrderItem(
+          orderItemModel.id,
+          orderItemModel.name,
+          orderItemModel.price,
+          orderItemModel.product_id,
+          orderItemModel.quantity
+        );
+      })
+    );
     return order;
   }
 
   async findAll(): Promise<Order[]> {
     const orderModel = await OrderModel.findAll();
     return orderModel.map((orderModel) =>
-      new Order(orderModel.id, orderModel.customer_id, orderModel.items)
+      new Order(
+        orderModel.id,
+        orderModel.customer_id,
+        orderModel.items.map((orderItemModel) => {
+          return new OrderItem(orderItemModel.id, orderItemModel.name, orderItemModel.price, orderItemModel.product_id, orderItemModel.quantity);
+        })
+      )
     );
   }
 }
